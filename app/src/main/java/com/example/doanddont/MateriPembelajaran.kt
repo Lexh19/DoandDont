eb0db1e9cd806d566539f82686189c8246c3b600
akmalmaulanak <akmalkismoyo@gmail.com>
2024-05-16 20:26:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/doanddont/MateriPembelajaran.kt
@@ -0,0 +1,11 @@
+package com.example.doanddont
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MateriPembelajaran : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_materi_pembelajaran)
+    }
+}
